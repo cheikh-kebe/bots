7cf55d170d11954d51296fc53fceac29c35232e7
cheikh <cheikh.kebe.01@gmail.com>
2021-05-03 09:28:35 +0200

    maj

--- lib/ruby_doc_creator.rb
+++ lib/ruby_doc_creator.rb
@@ -4,7 +4,7 @@ def get_folder_name
  if ARGV.empty?
   puts  "merci de renseigner le nom du dossier à créer"
   exit
- end#End of the fonction
+ end  #End of the fonction
   Dir.mkdir("/home/cheikh/HP/#{folder_name}")#fonction Dir.mkdir permet de créer un dossier en definissant un chemin
   
   app = File.open("/home/cheikh/HP/#{folder_name}/app.rb", "a")
@@ -13,16 +13,16 @@ def get_folder_name
   puts "require 'pry'"
   app.close
 
-  read = File.open("/home/cheikh/HP/#{folder_name}/README.md", "a")
+  read = File.open("/home/cheikh/HP/#{folder_name}/README.md", "a")#creation du fichier README.md
   read.puts "read me vièrge"
   read.close
 
-  gemfile = File.open("/home/cheikh/HP/#{folder_name}/Gemfile", "a")#create a file in the current folder
+  gemfile = File.open("/home/cheikh/HP/#{folder_name}/Gemfile", "a")#create a Gemfile in the current folder
   gemfile.puts"source 'https://rubygems.org'"
 
   gemfile.puts"ruby '2.7.1'"
   gemfile.puts"gem 'rubocop'"
-  gemfile.puts"gem 'dotenv'"
+  gemfile.puts"gem 'dotenv'"        #==> write all the gems in the Gemfile
   gemfile.puts"gem 'pry'"
   gemfile.puts"gem 'rspec'"
   gemfile.close
@@ -34,15 +34,17 @@ def get_folder_name
   env = File.open("/home/cheikh/HP/#{folder_name}/.env", "a")#create a file in the current folder
   env.close
 
-  Dir.chdir("/home/cheikh/HP/#{folder_name}")#change l'emplacement des différents 
+  Dir.chdir("/home/cheikh/HP/#{folder_name}")#change l'emplacement du dossier 
   puts system("git init")
-  puts system("rspec --init")
+  puts system("rspec --init")     #launch all the init and install all gems needed
   puts system("bundle install")
   
-  Dir.mkdir("/home/cheikh/HP/#{folder_name}/lib")
+  Dir.mkdir("/home/cheikh/HP/#{folder_name}/lib")#create a new folder lib
 
-  Dir.chdir("/home/cheikh/HP/#{folder_name}/lib")
-  app = File.open("/home/cheikh/HP/#{folder_name}/lib/app.rb", "a")
+  Dir.mkdir("/home/cheikh/HP/#{folder_name}/db")#create a new folder database 
+
+  Dir.chdir("/home/cheikh/HP/#{folder_name}/lib")#change directory 
+  app = File.open("/home/cheikh/HP/#{folder_name}/lib/app.rb", "a")#create a new ruby app.rb
   app.close
 
   app = File.open("/home/cheikh/HP/#{folder_name}/lib/app.rb", "a")
